1185a7fa203d67413d99b839b34e696e942f55d0
felixhackhack <73226682+felixhackhack@users.noreply.github.com>
2020-10-21 15:14:47 +0200

    Update requestAirtable.js

--- functions/requestAirtable.js
+++ functions/requestAirtable.js
@@ -3,22 +3,23 @@ require('dotenv').config()
 
 
 async function requestAirtable(tableName) {
-    
-    const airtable = new AirtablePlus({
-        baseID: process.env.BASE_NAME,
-        apiKey: process.env.API_KEY,
-        tableName: tableName,
-    });
-    
-    try {
-        const readRes = await airtable.read({
-            maxRecords: 100
-        });
-        return readRes;
-    }
-    catch(e) {
-        console.error(e);
-    }
+    
+    const airtable = new AirtablePlus({
+        baseID: process.env.BASE_NAME,
+        apiKey: process.env.API_KEY,
+        tableName: tableName,
+    });
+    
+    try {
+        const readRes = await airtable.read({
+            maxRecords: 100,
+            sort: [{field: 'index', direction: "desc"}]
+        });
+        return readRes;
+    }
+    catch(e) {
+        console.error(e);
+    }
 };
 
 module.exports = requestAirtable;
